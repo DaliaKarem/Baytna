5ca5db634c9545e87df665b21d7ad564a2662576
Dalia Karem <daliakarem0027@gmail.com>
2024-09-21 19:02:41 +0300

    Arch

--- app/src/main/java/com/example/baytna/Activity/MainActivity.kt
+++ app/src/main/java/com/example/baytna/Activity/MainActivity.kt
@@ -1,16 +1,13 @@
-package com.example.baytna
+package com.example.baytna.Activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.baytna.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
     }
 }
